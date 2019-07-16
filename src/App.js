b5ff47ea5bebf11c439fa14459aeee53b988efcd
sbhalla98 <sonalibhalla98@gmail.com>
2019-07-16 11:03:00 +0530

    header set

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-
-import './App.css';
-
-function App() {
+import React,{Component} from 'react';
+class App extends Component{
+render() {
   return (
-    
-   <div>
-     
-   </div>
+    <div>
+
+      Phone Directory
+    </div>
   );
 }
-
+}
 export default App;

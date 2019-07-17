bff225f190048c52c82a8a3a86f477f7ae21479d
sbhalla98 <sonalibhalla98@gmail.com>
2019-07-17 15:48:34 +0530

    static data adding

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React,{Component} from 'react';
+import Header from './Header.js'
+import './app.css';
+
 class App extends Component{
 render() {
+  let subscriber=[
+    {
+      id:1,
+      name:"sonali",
+      phone:"9569545786"
+    },
+    {
+      id:2,
+      name:"shilpa",
+      phone:"9569545796"
+
+    },
+    {
+      id:3,
+      name:"shilpa",
+      phone:"9569545796"
+
+    }
+  ]
   return (
     <div>
-
-      Phone Directory
-    </div>
+      <Header />
+      <button className="btn" id="add-btn">Add</button>
+    
+      <div className="label">
+        <label>name</label>
+        <label style={{marginLeft : '180px'}}>phone</label>
+        </div>
+        {
+      subscriber.map(arrayElement => {
+      return <div style={{position:'relative',top:'50px',left:'30px',fontWeight:'bolder'}} key={arrayElement.id}  className="row">
+      	<div className="col-lg-2">{arrayElement.name}</div>
+        <div className="col-lg-2">{arrayElement.phone}</div>
+       <div className="col-lg-8" style={{marginTop:'1px'}}><button className="delete-btn">DELETE</button></div>
+      </div>
+      })
+      }
+      </div> 
+     
   );
 }
 }
---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './header.css';
+
+const Header = function(){
+    return (
+    <div className="header">
+      Phone Directory
+      </div>
+    );
+}
+
+
+
+export default  Header ;
